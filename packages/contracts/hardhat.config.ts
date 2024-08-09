e53981d721cf03895d637f5693a0533e3d995b26
malawad <139969752+sixthCow@users.noreply.github.com>
2024-08-09 16:57:16 +0300

    added virtutal testnet

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -5,6 +5,9 @@ import "./tasks";
 
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as tenderly from "@tenderly/hardhat-tenderly";
+
+tenderly.setup({ automaticVerifications: true });
 
 const accounts =
   process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
@@ -44,6 +47,13 @@ const config: HardhatUserConfig = {
       url: "https://sepolia.optimism.io" || "",
       accounts: accounts,
       chainId: 11155420,
+    },
+
+    virtual_base: {
+      url: "https://virtual.base.rpc.tenderly.co/1e50f9a0-82d7-4088-8039-0ed3192bd956",
+      accounts: accounts,
+      chainId: 84533,
+
     },
   },
   etherscan: {
@@ -90,6 +100,12 @@ const config: HardhatUserConfig = {
         },
       },
     ],
+  },
+
+  tenderly: {
+    // https://docs.tenderly.co/account/projects/account-project-slug
+    project: process.env.TEND_ACCOUNT,
+    username: process.env.TEND_NAME,
   },
 };
 
